bbbab598176fea2d1d540c54c229b47a155692ed
Rachel <me@rcabot.com>
2015-07-12 22:54:25 +0100

    Added SpawnUnit action framework.

---
+++ Assets/scripts/lockstep-engine/code/Actions/SpawnUnit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+[Serializable]
+public class SpawnUnit : IAction
+{
+	int owningPlayer;
+	int buildingID;
+	
+	public SpawnUnit (int owningPlayer, int buildingID) {
+		this.owningPlayer = owningPlayer;
+		this.buildingID = buildingID;
+	}
+	
+	public void ProcessAction() {
+		//<<>> b = SceneManager.Manager.<<>>(owningPlayer, buildingID);
+		//b.SpawnUnit();
+	}
+
+}
